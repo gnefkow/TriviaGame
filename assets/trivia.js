b892a8494489b193b76dc9d16f6ff50c84943aa7
Kyle Becker <gnefkow87@gmail.com>
2019-11-06 17:11:46 -0500

    Created question object and loop to create a question array from it

--- assets/trivia.js
+++ assets/trivia.js
@@ -7,27 +7,67 @@
     var timerEL = document.getElementById(`timer`);
 
 //MECHANICS
-  // CLOCK  
-    var intervalID;  
-    var clockRunning = false;
-    var timeLeft;
-    
 
-// Utility Functions:
-  function gameTime() {gameOver();};
+// QUESTION BANK ---------- ---------- ---------- ---------- ---------- 
+var questionBank = {
+  1 : [
+    "Beat on the brat. Beat on the brat. Beat on the brat with a...",
+    "Pork-Pie Hat!",
+    "Baseball Bat!",
+    1
+  ],
+  "2" : [
+    "A message to you",
+    "Tony",
+    "Baby",
+    "Rudy",
+    3
+  ],
+  "3" : [
+    "The Guns of...",
+    "The gangsters",
+    "Navarone",
+    "Rudy",
+    2
+  ]
+};
+
+//Generate Question Arrays
+  //Create an open array for everything:
+    var questionNumber = [];
   
-  function runClock() {
-    if (!clockRunning){
-      timeLeft = 10;
-      intervalId = setInterval(countDown, 1000);
-      clockRunning = true;
+  // Generate the number of questions based off of the Question Bank Object:
+  for (var key in questionBank) {
+    if (questionBank.hasOwnProperty(key)) {
+      questionNumber.push(key);
+      questionNumber.push(questionBank[key]);
     };
   };
 
+  // for (var i = 0; i < questionNumbers.length; i++) {
+  //     var currentQuestion = questionNumbers[i];
+  //     var question = questionBank.currentQuestion;
+  //   // currentQuestion.push(i);
+
+  // }
+    
+
+// TIMER ---------- ---------- ---------- ---------- ---------- 
+  var intervalID;  
+  var clockRunning = false;
+  var timeLeft;  
+
+  function runClock() {
+    if (!clockRunning){
+      timeLeft = 40;  //set the timer here
+      intervalId = setInterval(countDown, 1000);
+      clockRunning = true;
+      };
+    };
+
   function stopClock(){ 
     clearInterval(intervalId);
-    timeLeft = 10;
-  };
+    };
 
   function countDown() {
     timeLeft--;
@@ -36,40 +76,49 @@
       clockRunning = false;
       stopClock();
       gameOver();
-    ;}
-  }
+      };
+    };
   
+
+
+
+// ////////// ========== ========== GAME STATES ========== ========== ////////// //
+
+// ---------- ---------- START PAGE ---------- ---------- //
+function startPage() {
+  // Displays Intro Text:
+    mainTextEL.innerHTML = "Click to Start Game";
+    questionChoices.style.display = "none";
+  // START BUTTON STARTS THE GAME (calls the startGame function):
+    startButtonEL.onclick = startGame;
+  };
+
 
   // ----- ----- START THE GAME: ----- ----- //
   function startGame() { 
     console.log("START!")
-   // Hide the Start Button:
+   // Hide the "Start Page" or "Game Over" pages:
       startButtonEL.style.display = "none";
+
+  // Start the CLock:    
       runClock();
       countDown();
+      timerEL.style.display = "block";
+
   // Start the Game:
       displayQuestions();
   };
 
 
-
-// ---------- ---------- START PAGE ---------- ---------- //
-  function startPage() {
-    // Displays Intro Text:
-      mainTextEL.innerHTML = "Click to Start Game";
-      questionChoices.style.display = "none";
-    // START BUTTON STARTS THE GAME (calls the startGame function):
-      startButtonEL.onclick = startGame;
-    };
-
-
 // ---------- ---------- DISPLAY QUESTIONS ---------- ---------- //
-  function displayQuestions() {
-    //Display Question:
-      mainTextEL.innerHTML = "Here is the first question";
-    // Display answer choices:
-      questionChoices.style.display = "block";
-  };
+var questionNumberPlay;
+function displayQuestions(){
+  //Display Question:
+    mainTextEL.innerHTML = questionNumber[1][0];
+ // Display answer choices:
+     questionChoices.style.display = "block";
+
+};
 
     
     
@@ -98,6 +147,10 @@
     //replay Button
     startButtonEL.style.display = "block";
     startButtonEL.innerHTML = "play again";
+
+    //Hide Game Play Elements:
+      timerEL.style.display = "none";
+      questionChoices.style.display = "none";
   };
     // function: executes after last question or when the timer is up
     // display sum of correct answers
